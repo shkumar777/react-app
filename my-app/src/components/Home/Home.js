9a64ab557d2b453949003ab70e0bb71aff158cc2
Sharath Kumar <Shkumar@synamedia.com>
2023-12-12 10:49:45 +0530

    Added Home Page

---
+++ my-app/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+const Home = ()=>{
+    return (
+        <div className="responsive-image-container" >
+        <img className="responsive-image" src="kayak.jpeg" alt='logo'></img>
+        <h1 className='text-overlay'>KAYAK AT THE BEST PLACE IN WORLD</h1>
+        </div>
+    )
+}
+
+export default Home;
