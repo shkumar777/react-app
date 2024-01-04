f469d5663f349a030a55da3b866c1047d1c3a4ea
Sharath Kumar <Shkumar@synamedia.com>
2024-01-04 09:26:01 +0530

    Scafolding project

--- my-app/src/components/Home/Home.js
+++ my-app/src/components/Home/Home.js
@@ -3,9 +3,16 @@ import './Home.css'
 
 const Home = ()=>{
     return (
-        <div className="responsive-image-container" >
-        <img className="responsive-image" src="kayak.jpeg" alt='logo'></img>
-        <h1 className='text-overlay'>KAYAK AT THE BEST PLACE IN WORLD</h1>
+        <div className="responsive-image-container">
+                
+                <nav className='nav-style'>
+                    <a href='#'>About</a>
+                    <a href='#'>Day Out</a>
+                    <a href='#'>Resort</a>
+                    <a href='#'>Contact Us</a>
+                </nav>
+                <img className="responsive-image" src="kayak.jpeg" alt='logo'></img>
+                <h1 className='text-overlay'>KAYAK AT THE BEST PLACE IN THE WORLD</h1>
         </div>
     )
 }
